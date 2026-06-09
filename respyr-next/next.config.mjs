357739349b4d312a5f32f9ad357916b94b764895
Humors <125995853+humors2025@users.noreply.github.com>
2026-06-09 15:12:45 +0530

    Comment out nextConfig in next.config.mjs
    Commented out the entire nextConfig object in next.config.mjs.

--- respyr-next/next.config.mjs
+++ respyr-next/next.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // Static HTML export -> produces an `out/` folder suitable for S3 + CloudFront.
-  output: 'export',
   images: { unoptimized: true },
 };
-
 export default nextConfig;
+
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   // Static HTML export -> produces an `out/` folder suitable for S3 + CloudFront.
+//   output: 'export',
+//   images: { unoptimized: true },
+// };
+
+// export default nextConfig;
